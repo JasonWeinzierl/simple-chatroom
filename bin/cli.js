d91c55f36eba428e71a8c73ef01da216018b7074
Jason Weinzierl <weinzierljason@gmail.com>
2023-07-16 21:35:09 -0500

    chore: upgrade to ES modules

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,8 @@
 #!/usr/local/bin/node
 
-const readline = require('readline');
+import { createInterface } from 'readline';
+import startServer from '../lib/server.js';
+import startClient from '../lib/client.js';
 
 const [,, ...args] = process.argv;
 
@@ -11,50 +13,49 @@ if (args.length > 0) {
         case 'S':
         case 'server':
             console.log("Starting server.  Run `npm start c` in another shell to start a client.");
-            require('../lib/server.js');
+            startServer();
             break;
         case 'c':
         case 'C':
         case 'client':
             console.log("Starting client.");
-            require('../lib/client.js');
+            startClient();
             break;
         default:
             console.log("Incorrect command line argument");
             break;
     }
-    return;
+} else {
+    // get input from terminal
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    rl.setPrompt("Would you like to start server or client? (s/c): ");
+
+    rl.prompt();
+
+    rl.on('line', line => {
+        switch (line) {
+            case 's':
+            case 'S':
+            case 'server':
+                rl.close();
+                console.log("Starting server.  Run `npm start c` in another shell to start a client.");
+                require('../lib/server.js');
+                break;
+            case 'c':
+            case 'C':
+            case 'client':
+                rl.close();
+                console.log("Starting client.");
+                require('../lib/client.js');
+                break;
+            default:
+                rl.setPrompt("Incorrect input.  (s/c): ");
+                rl.prompt();
+                break;
+        }
+    });
 }
-
-// get input from terminal
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-rl.setPrompt("Would you like to start server or client? (s/c): ");
-
-rl.prompt();
-
-rl.on('line', line => {
-    switch (line) {
-        case 's':
-        case 'S':
-        case 'server':
-            rl.close();
-            console.log("Starting server.  Run `npm start c` in another shell to start a client.");
-            require('../lib/server.js');
-            break;
-        case 'c':
-        case 'C':
-        case 'client':
-            rl.close();
-            console.log("Starting client.");
-            require('../lib/client.js');
-            break;
-        default:
-            rl.setPrompt("Incorrect input.  (s/c): ");
-            rl.prompt();
-            break;
-    }
-});
